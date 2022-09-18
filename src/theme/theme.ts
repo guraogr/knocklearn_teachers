47b56ef6086c94b1b5d2496477c54619659baab8
guraogr <guraogr@gmail.com>
2022-09-18 22:25:23 +0900

    add: extendThemeにbrandを追加

---
+++ src/theme/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      50: '#F6FAFF',
+      100: '#E1F0FF',
+      200: '#CDE6FF',
+      300: '#B4D6F8',
+      400: '#98C2EC',
+      500: '#729ECA',
+      600: '#517DA8',
+      700: '#365E86',
+      800: '#204264',
+      900: '#102942',
+    },
+  },
+})
