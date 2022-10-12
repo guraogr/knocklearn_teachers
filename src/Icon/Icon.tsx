9ef99c2bfb67ec633e49df8a0bf0c95f51f9f1a9
guraogr <guraogr@gmail.com>
2022-10-12 14:28:54 +0900

    add: マルバツ三角アイコンの作成

---
+++ src/Icon/Icon.tsx
@@ -0,0 +1,45 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const DoubleCircle = (props: IconProps) => (
+  <Icon viewBox="0 0 14 14" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 7C12 9.76142 9.76142 12 7 12C4.23858 12 2 9.76142 2 7C2 4.23858 4.23858 2 7 2C9.76142 2 12 4.23858 12 7ZM14 7C14 10.866 10.866 14 7 14C3.13401 14 0 10.866 0 7C0 3.13401 3.13401 0 7 0C10.866 0 14 3.13401 14 7ZM8.5 7C8.5 7.82843 7.82843 8.5 7 8.5C6.17157 8.5 5.5 7.82843 5.5 7C5.5 6.17157 6.17157 5.5 7 5.5C7.82843 5.5 8.5 6.17157 8.5 7ZM10.5 7C10.5 8.933 8.933 10.5 7 10.5C5.067 10.5 3.5 8.933 3.5 7C3.5 5.067 5.067 3.5 7 3.5C8.933 3.5 10.5 5.067 10.5 7Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
+
+export const SingleOutlineCircle = (props: IconProps) => (
+  <Icon viewBox="0 0 14 14" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7 12C9.76142 12 12 9.76142 12 7C12 4.23858 9.76142 2 7 2C4.23858 2 2 4.23858 2 7C2 9.76142 4.23858 12 7 12ZM7 14C10.866 14 14 10.866 14 7C14 3.13401 10.866 0 7 0C3.13401 0 0 3.13401 0 7C0 10.866 3.13401 14 7 14Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
+
+export const OutlineTriangle = (props: IconProps) => (
+  <Icon viewBox="0 0 16 13" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.99979 0.583313L0.927246 12.8333H15.0723L7.99979 0.583313ZM7.99979 4.58331L4.39135 10.8333H11.6082L7.99979 4.58331Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
+
+export const Close = (props: IconProps) => (
+  <Icon viewBox="0 0 12 12" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.26828 6.14581L11.3737 2.04041L9.95947 0.626192L5.85406 4.7316L1.74869 0.626221L0.334473 2.04043L4.43985 6.14581L0.334473 10.2512L1.74869 11.6654L5.85406 7.56003L9.95947 11.6654L11.3737 10.2512L7.26828 6.14581Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
