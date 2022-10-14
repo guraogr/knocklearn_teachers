bdb47c699ecd7a16272ab706fc07f488dbe08a3e
guraogr <guraogr@gmail.com>
2022-10-15 04:28:33 +0900

    add: api.tsの作成

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,3 +1,39 @@
+export interface LecturerExperience {
+  fieldId: string;
+  lecturer_exp_type: string[];
+  lecturer_exp_year: string[];
+  work_desc: string;
+  work_school_name: string;
+}
+
+export interface Photo {
+  fieldId: string;
+  photo: {
+    height: number;
+    url: string;
+    width: number;
+  };
+}
+
+export interface StudyAbroadExp {
+  abroad_exp_country: string;
+  abroad_exp_type: string[];
+  fieldId: string;
+}
+
+export interface TeachSubject {
+  fieldId: string;
+  teach_level: string[];
+  teach_school_year: string[];
+  teach_subject: string[];
+}
+
+export interface WorkDate {
+  day_of_week: string[];
+  fieldId: string;
+  times: string[];
+}
+
 export interface Teacher {
   age: number;
   can_exam_support: string[];
@@ -16,13 +52,7 @@ export interface Teacher {
   id: string;
   industory: string;
   job_category: string;
-  lecturer_experience: {
-    fieldId: string;
-    lecturer_exp_type: string[];
-    lecturer_exp_year: string[];
-    work_desc: string;
-    work_school_name: string;
-  }[];
+  lecturer_experience: LecturerExperience[];
   lecturer_pass_record: string;
   middle_school_name: string;
   middle_school_type: string[];
@@ -31,37 +61,17 @@ export interface Teacher {
   my_study_tendency: string[];
   name: string;
   name_furigana: string;
-  photo: {
-    fieldId: string;
-    photo: {
-      height: number;
-      url: string;
-      width: number;
-    };
-  }[];
+  photo: Photo[];
   pr_video: string;
   publishedAt: string;
   qualification: string;
   revisedAt: string;
   self_pr: string;
-  study_abroad_exp: {
-    abroad_exp_country: string;
-    abroad_exp_type: string[];
-    fieldId: string;
-  }[];
+  study_abroad_exp: StudyAbroadExp[];
   talk_theme: string;
-  teach_subject: {
-    fieldId: string;
-    teach_level: string[];
-    teach_school_year: string[];
-    teach_subject: string[];
-  }[];
+  teach_subject: TeachSubject[];
   transfer_class: string[];
   truancy_support: string[];
   updatedAt: string;
-  work_date: {
-    day_of_week: string[];
-    fieldId: string;
-    time: string[];
-  }[];
+  work_date: WorkDate[];
 }
