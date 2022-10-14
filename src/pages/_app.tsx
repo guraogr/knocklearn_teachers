c1b9a5ebead38477577cd2e95fb3937a37da355e
guraogr <guraogr@gmail.com>
2022-10-15 04:29:42 +0900

    add: framer-motionでアニメーションを付与

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,14 @@ import '@fontsource/noto-sans-jp/700.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { theme } from '../theme/theme';
+import { AnimatePresence } from 'framer-motion';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter onExitComplete={() => window.scrollTo(0, 0)}>
+        <Component key={router.asPath} {...pageProps} />
+      </AnimatePresence>
     </ChakraProvider>
   );
 }
