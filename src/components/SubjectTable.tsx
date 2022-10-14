a4dd2aa425a113180bcbf8f9ffac4b19e3c668f9
guraogr <guraogr@gmail.com>
2022-10-15 04:31:49 +0900

    add: apiデータをSubjectTable.tsxに挿入

--- src/components/SubjectTable.tsx
+++ src/components/SubjectTable.tsx
@@ -1,7 +1,22 @@
 import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import { SingleOutlineCircle } from '../Icon/Icon';
+import { Close, DoubleCircle, OutlineTriangle, SingleOutlineCircle } from '../Icon/Icon';
+import { subjectNames, teachLevels } from '../libs/tableData';
+import { TeachSubject } from '../types/api';
 
-const SubjectTable = () => {
+interface Props {
+  teach_subject: TeachSubject[];
+}
+
+const SubjectTable = ({ teach_subject }: Props) => {
+  const renderTeachLevel = (teachLevel: string) => {
+    if (teachLevel === teachLevels[0]) {
+      return <DoubleCircle color={'primary'} boxSize={'18px'} mr={'4px'} />;
+    } else if (teachLevel === teachLevels[1]) {
+      return <SingleOutlineCircle color={'primary'} boxSize={'16px'} mr={'4px'} />;
+    } else {
+      return <OutlineTriangle color={'secondary'} boxSize={'18px'} mr={'4px'} />;
+    }
+  };
   return (
     <TableContainer>
       <Table
@@ -26,94 +41,39 @@ const SubjectTable = () => {
             </Th>
           </Tr>
           <Tr>
-            <Th
-              textAlign="center"
-              color="white"
-              borderBottomWidth={0}
-              borderColor="rgba(219,219,219,0.2)"
-              borderRightWidth={'1px'}
-            >
-              国語
-            </Th>
-            <Th
-              textAlign="center"
-              color="white"
-              borderBottomWidth={0}
-              borderColor="rgba(219,219,219,0.2)"
-              borderRightWidth={'1px'}
-            >
-              数学
-            </Th>
-            <Th
-              textAlign="center"
-              color="white"
-              borderBottomWidth={0}
-              borderColor="rgba(219,219,219,0.2)"
-              borderRightWidth={'1px'}
-            >
-              理科
-            </Th>
-            <Th
-              textAlign="center"
-              color="white"
-              borderBottomWidth={0}
-              borderColor="rgba(219,219,219,0.2)"
-              borderRightWidth={'1px'}
-            >
-              社会
-            </Th>
-            <Th
-              textAlign="center"
-              color="white"
-              borderBottomWidth={0}
-              borderColor="rgba(219,219,219,0.2)"
-              borderRightWidth={'1px'}
-            >
-              英語
-            </Th>
+            {subjectNames.map((subjectNames, index) => (
+              <Th
+                key={index}
+                textAlign="center"
+                color="white"
+                borderBottomWidth={0}
+                borderColor="rgba(219,219,219,0.2)"
+                borderRightWidth={index === subjectNames.length - 1 ? '0' : '1px'}
+              >
+                {subjectNames}
+              </Th>
+            ))}
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              中学一年生
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border">
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              中学一年生
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-            <Td textAlign="center" borderColor="border">
-              <SingleOutlineCircle color={'primary'} boxSize={'14px'} mr={'4px'} />
-            </Td>
-          </Tr>
+          {teach_subject.map((data, index) => {
+            return (
+              <Tr key={`${data.fieldId}-${index}`}>
+                <Td textAlign="center" borderColor="border" borderRightWidth={'1px'}>
+                  {data.teach_school_year}
+                </Td>
+                {subjectNames.map((subjectName, index) => (
+                  <Td key={index} textAlign="center" borderColor="border" borderRightWidth={'1px'}>
+                    {data.teach_subject.includes(subjectName) ? (
+                      <>{renderTeachLevel(data.teach_level[0])}</>
+                    ) : (
+                      <Close color={'border'} boxSize={'14px'} mr={'4px'} />
+                    )}
+                  </Td>
+                ))}
+              </Tr>
+            );
+          })}
         </Tbody>
       </Table>
     </TableContainer>
